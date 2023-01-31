109766204d5e2a84656a72fe75362a6e4da3619d
Jordan Capital <dicksonmokua19@gmail.com>
2023-01-31 17:32:26 -0600

    Removed the last food line 67, Added food at the beginning line 76, Removed fthe first food using Array.shift

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -53,7 +53,8 @@ animalArray.push('penguin');
 console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
-
+foodArray.push('beef');
+console.log('Added food to the end', foodArray);
 
 
 // Example: Remove the last animal by using Array.pop
@@ -63,6 +64,9 @@ console.log('The animals are now', animalArray);
 
 // 4.b. TODO: Remove the food at the end of your array & 
 //      log both the food removed and the updated array
+let removedFood = foodArray.pop();
+console.log('Removed the last food',removedFood);
+console.log('The food now is', foodArray);
 
 
 // Example: Add an animal to the beginning using Array.unshift
@@ -70,6 +74,8 @@ animalArray.unshift('walrus');
 console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
+foodArray.unshift('Sausage');
+console.log(`Added food at the beginning: ${foodArray}`);
 
 
 // Example: Remove the first animal using Array.shift
@@ -79,6 +85,9 @@ console.log('The animals are now', animalArray);
 
 // 4.d TODO: Remove the food at the beginning of your array & 
 //     log both the food removed and the updated array
+removedFood = foodArray.shift();
+console.log('Removed the first food', removedFood);
+console.log('The food now is',foodArray);
 
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
